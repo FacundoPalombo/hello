908ae31f7402811dbc0211ed4154f7a329298981
Facundo Palombo facundo.palombo@gmail.com <facundo.palombo@gmail.com>
2024-08-26 02:58:52 +0200

    feat: add work experiences

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero.tsx";
 import "./App.css";
+import WorkExperiences from "./components/WorkExperiences.tsx";
 
 function App() {
   return (
     <main>
       <Hero />
+      <WorkExperiences></WorkExperiences>
     </main>
   );
 }
