30027b53f1e0ceafe6192323a88cc237539cb162
Facundo Palombo facundo.palombo@gmail.com <facundo.palombo@gmail.com>
2024-08-29 20:12:53 +0200

    feat: add italian translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,15 +3,15 @@ import { initReactI18next } from "react-i18next";
 import detector from "i18next-browser-languagedetector";
 
 import translationENG from "./locales/en/translation.json";
+import translationITA from "./locales/it/translation.json";
 
 i18n
   .use(detector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    // the translations
-    // (tip move them in a JSON file and import them,
-    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    supportedLngs: ["es", "en", "it"],
     resources: {
+      it: translationITA as ResourceLanguage,
       en: translationENG as ResourceLanguage,
     },
     fallbackLng: "es",
